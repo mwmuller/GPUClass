661d53c9b4bac4d11258ee1aa1b2f69f787be78c
Michael Muller <mwmuller94@gmail.com>
2021-09-16 14:56:34 -0400

    completed 1d map array creation

--- DAlgo/DAlgo/kernel.cu
+++ DAlgo/DAlgo/kernel.cu
@@ -5,14 +5,15 @@
 #include <string>
 #include <stdlib.h>
 #include <cmath>
+#include <iostream>
 using namespace std;
 
 // Number of vertices in the graph
 #define V 9
-void convertTo1DArray();
+void create1DMap(int[], int, int*&, int*&);
 // A utility function to find the vertex with minimum distance value, from
 // the set of vertices not yet included in shortest path tree
-void minDistance(int dist[], bool sptSet[], int* &min_index)
+void minDistance(int* dist, bool* sptSet, int *&min_index)
 {
 
 	// Initialize min value
@@ -20,7 +21,7 @@ void minDistance(int dist[], bool sptSet[], int* &min_index)
 
 	for (int v = 0; v < V; v++)
 		if (sptSet[v] == false && dist[v] <= min)
-			min = dist[v], min_index = new int(v);
+			min = dist[v], *min_index = v;
 }
 
 __global__ void print2dArr(int *arr)
@@ -38,26 +39,32 @@ __global__ void calcShortest(int *graph, int* dist, bool* sptSet, int minIndex)
 {
 	// We need to have each thread check a different dist location. If they all check the name location, then
 	// We will have the highest thread < V update the value in dist arr
+
 	int thread = blockIdx.x*blockDim.x + threadIdx.x;
 	if (thread < V)
 	{
-	//	if (!sptSet[thread] && graph[minIndex][thread] && dist[minIndex] != INT_MAX
-			//&& ((dist[minIndex] + graph[minIndex][thread]) < dist[thread]))
-			//dist[thread] = dist[thread] + graph[minIndex][thread];
+		int graphVal = graph[minIndex*V + thread];
+		int distVal = dist[minIndex];
+			if (!sptSet[thread] && graphVal && dist[minIndex] != INT_MAX
+				&& ((dist[minIndex] + graphVal) < dist[thread]))
+				dist[thread] = dist[minIndex]; + graphVal;
 	}
 }
 
 // A utility function to print the constructed distance array
-__global__ void printSolution(int* dist)
+void printSolution(int* dist)
 {
-	printf("Vertex \t Distance from Source\n");
-	int i = blockIdx.x * blockDim.x + threadIdx.x;
-	printf(" \t\t%d\n", dist[i]);
+	for(int i = 0; i < V - 1; i++)
+	{
+		int getthis = sizeof(dist);
+		printf("Vertex \t Distance from Source\n");
+		printf(" \t\t%d\n", dist[i]);
+	}
 }
 
 // Function that implements Dijkstra's single source shortest path algorithm
 // for a graph represented using adjacency matrix representation
-int* dijkstra(int *graph, int src)
+void dijkstra(int *graph, int src)
 {
 	cudaError_t cudaError;
 
@@ -66,7 +73,7 @@ int* dijkstra(int *graph, int src)
 	int test2dArr[sizeof(dSize)];
 	int *dev_graph;
 	int* dist = new int[V]; // The output array.  dist[i] will hold the shortest
-	int* dev_Dist = { 0 };
+	int* dev_Dist;
 
 	bool* sptSet = new bool[V]; // sptSet[i] will be true if vertex i is included in shortest
 
@@ -82,8 +89,7 @@ int* dijkstra(int *graph, int src)
 	dist[src] = 0;
 
 	// Find shortest path for all vertices
-	for (int count = 0; count < V - 1; count++) {
-		int *u = 0;
+		int *u = new int(0);
 		int minIndex = 0;
 		// Pick the minimum distance vertex from the set of vertices not
 		// yet processed. u is always equal to src in the first iteration.
@@ -104,41 +110,33 @@ int* dijkstra(int *graph, int src)
 			fprintf(stderr, "%s", cudaError);
 		}
 		cudaMalloc((void**)&dev_graph, dSize);
-		cudaMemcpy2D(dev_graph, dSize, graph, V, V, V, cudaMemcpyHostToDevice);
-
-		cudaMemcpy(test2dArr, dev_graph, dSize, cudaMemcpyDeviceToHost);
+		cudaMemcpy(dev_graph, graph, dSize, cudaMemcpyHostToDevice);
 		
 		calcShortest<<<blocks, threads >>>(dev_graph, dev_Dist, sptSet, minIndex);
-
-		cudaDeviceSynchronize();
 		cudaMemcpy(dist, dev_Dist, dSize, cudaMemcpyDeviceToHost);
+
+		cudaFree(dev_Dist);
+
+		printSolution(dist);
 		// Update dist[v] only if is not in sptSet, there is an edge from
 		// u to v, and total weight of path from src to  v through u is
 		// smaller than current value of dist[v]
-
-	}
-
-
-
-	return dist;
 }
 
 // driver program to test above function
 int main()
 {
-	size_t dSize = V * V * sizeof(int);
-	int* hostDist = (int*)malloc(dSize);
-
+	int* hostDist = { 0 };
+	int* arrPitch = new int;
 	/* Let us create the example graph discussed above */
-	int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, // This might need to be converted to a 1d Matrix for simplicity
-						{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-						{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-						{ 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-						{ 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-						{ 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-						{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-						{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-						{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+	int graph[] = { 4, 0, 0, 0, 0, 0, 8, 0,
+					8, 0, 0, 0, 0, 11, 0,
+					7, 0, 4, 0, 0, 2,
+					9, 14, 0, 0, 0,
+					10, 0, 0, 0,
+					2, 0, 0,
+					1, 6,
+					7}; // this is out simplified graph
 
 	// We can convert the 2d array into a 1d array in qhich we construct a map. 
 	// We can create an array of size ArrSize ==> [(1+n)n]/2 n is the width and 
@@ -147,37 +145,43 @@ int main()
 
 	// ceil(sqrt(n*2)) will provide us the width/height of our 2d array.
 
-	convertTo1DArray();
-	memcpy(hostDist, graph, dSize);
-	cudaMemcpy2D(hostDist, dSize, graph, dSize, V, V, cudaMemcpyHostToHost);
-	int* sol = dijkstra(hostDist, 0);
-
-	printSolution << <1, V >> > (sol);
+	create1DMap(graph, sizeof(graph), hostDist, arrPitch);
+	int getit = hostDist[7];
+	dijkstra(hostDist, 0);
+	
 	cudaDeviceSynchronize();
 
 	return 0;
 }
 
-int getMallocSize(int arrayPitch)
+void create1DMap(int inputArr[], int inputSize, int *&outputArr, int *&ouputPitch)
 {
-	int size = pow(arrayPitch, 2.0) * sizeof(int);
-	return size;
-}
-void convertTo1DArray()
-{
-	int testArr[6] = { 1, 0, 2, 5, 4, 6 };
-	int elements = sizeof(testArr) / sizeof(int);
+	int elements = inputSize/ sizeof(int);
 	int arrPitch = ceil(sqrt(static_cast<double>(elements * 2)));
-	int totalSize = getMallocSize(arrPitch);
-	int *output = (int*)malloc(getMallocSize(arrPitch));
-	memset(output, 0, totalSize);
-	for (int i = 0; i < arrPitch; i++)
+	*ouputPitch = arrPitch;
+	int totalElements = pow(arrPitch, 2.0);
+	outputArr = new int[totalElements];
+	memset(outputArr, 0, totalElements * sizeof(int));
+	int topArr = 0, botArr = 0;
+	for (int i = 0; i < arrPitch - 1; i++)
 	{
-		float diagonal = floor(static_cast<float>(i)/ static_cast<float>((arrPitch + 1)));
-
-		for (int x = 0; x < arrPitch - 1; x++)
+		for (int x = 0; x < arrPitch - (i + 1); x++)
 		{
-			output[(x * 5) + (1 + i)] = testArr[(0 + x + i) % elements]; // currently working on a solution to this
+			outputArr[(i * (arrPitch + 1)) + x + 1] = inputArr[topArr];
+			topArr++;
 		}
+		for (int y = 0; y < arrPitch - (i + 1); y++)
+		{
+			outputArr[((y + i + 1) * arrPitch) + i] = inputArr[botArr];
+			botArr++;
+		}
+	}
+	for (int i = 0; i < totalElements; i++)
+	{
+		if (i % arrPitch == 0)
+		{
+			std::cout << endl;
+		}
+		std::cout << outputArr[i] << " ";
 	}
 }
